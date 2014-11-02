ea49cef31548a810085f9871e69be965e85b5999
Marco Ceppi <marco@ceppi.net>
2014-11-02 08:03:39 -0500

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+runsonubuntu.com README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_runsonubuntu.com_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'psycopg2',
+    'alembic',
+    'marshmallow',
+    ]
+
+setup(name='runsonubuntu',
+      version='0.0.1',
+      description='runsonubuntu.com',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='runsonubuntu',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = runsonubuntu:main
+      [console_scripts]
+      initialize_runsonubuntu_db = runsonubuntu.scripts.initializedb:main
+      """,
+      )
